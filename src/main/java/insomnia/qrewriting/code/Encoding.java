f018d959b03be0e298a806ea4572073223fe150f
zuri <webzuri@gmail.com>
2019-05-09 17:47:04 +0200

    Encoding, add add(Code, int), getMultiBase()

--- src/main/java/insomnia/qrewriting/code/Encoding.java
+++ src/main/java/insomnia/qrewriting/code/Encoding.java
@@ -23,6 +23,36 @@ public class Encoding extends ArrayList<NodeContext>
 		return new Interval(0, getTotalNbStates() - 1);
 	}
 
+	public void add(Code code, int val)
+	{
+		int[] multiBase = getMultiBase();
+		int   num       = val;
+		int   rest;
+
+		for (int i = multiBase.length - 1; i >= 0; i--)
+		{
+			final int base = multiBase[i];
+			num  += code.getCode(i);
+			rest  = num % base;
+			num  /= base;
+			code.setCode(i, rest);
+
+			if (num == 0)
+				break;
+		}
+	}
+
+	public int[] getMultiBase()
+	{
+		final int size  = this.size();
+		int       ret[] = new int[size];
+
+		for (int i = 0; i < size; i++)
+			ret[i] = get(i).getContext().size();
+
+		return ret;
+	}
+
 	/**
 	 * Calcule pour chaque position d'un code de l'encodage le format
 	 * d'affichage, c-à-d le nombre de chiffres à afficher pour chaque code
