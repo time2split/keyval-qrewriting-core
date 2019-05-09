39f7312f2e5f93a96418e6cf1c82f115a3831486
zuri <webzuri@gmail.com>
2019-05-09 17:52:27 +0200

    internal/TheDriver, add getAQueryEvaluator()

--- src/main/java/insomnia/qrewriting/database/driver/internal/TheDriver.java
+++ src/main/java/insomnia/qrewriting/database/driver/internal/TheDriver.java
@@ -1,8 +1,11 @@
 package insomnia.qrewriting.database.driver.internal;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 import insomnia.qrewriting.database.AbstractDriver;
 import insomnia.qrewriting.database.DriverException;
 import insomnia.qrewriting.database.driver.DriverQueryBuilder;
+import insomnia.qrewriting.database.driver.DriverQueryEvaluator;
 import insomnia.qrewriting.database.driver.DriverQueryManager;
 
 public class TheDriver extends AbstractDriver
@@ -17,5 +20,11 @@ public class TheDriver extends AbstractDriver
 	public DriverQueryManager getAQueryManager() throws DriverException
 	{
 		return new MyQueryManager(this);
+	}
+
+	@Override
+	public DriverQueryEvaluator getAQueryEvaluator() throws DriverException
+	{
+		throw new DriverException(new NotImplementedException(""));
 	}
 }
