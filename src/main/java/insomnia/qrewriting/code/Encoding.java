1cc869b252ef9f17f6e7ea4f03a550970218a38c
zuri <webzuri@gmail.com>
2019-05-09 17:48:33 +0200

    Encoding, getCodeFrom() optimization, @deprecated generateAllCodes()

--- src/main/java/insomnia/qrewriting/code/Encoding.java
+++ src/main/java/insomnia/qrewriting/code/Encoding.java
@@ -9,7 +9,6 @@ import insomnia.qrewriting.generator.NodeContext;
  * Encodage : signification des Codes. Suit la même indexation que les Codes.
  * 
  * @author zuri
- * 
  */
 public class Encoding extends ArrayList<NodeContext>
 {
@@ -61,15 +60,15 @@ public class Encoding extends ArrayList<NodeContext>
 	 */
 	public int[] getCodeFormat()
 	{
-		final int size = this.size();
-		int format[] = new int[size];
+		final int size     = this.size();
+		int       format[] = new int[size];
 
 		for (int i = 0; i < size; i++)
 		{
-			CodeContext c = this.get(i).getContext();
-			final int nbStates = c.size() - 1;
-			
-			if(nbStates > 0)
+			CodeContext c        = this.get(i).getContext();
+			final int   nbStates = c.size() - 1;
+
+			if (nbStates > 0)
 				format[i] = (int) (Math.floor(Math.log10(nbStates))) + 1;
 			else
 				format[i] = 1;
@@ -78,41 +77,43 @@ public class Encoding extends ArrayList<NodeContext>
 	}
 
 	/**
-	 * 
 	 * @param c
-	 *            Représentation d'un code
+	 *          Représentation d'un code
 	 * @return Le code dont la représentation numérique est $c
 	 */
 	public Code getCodeFrom(int c)
 	{
-		final Code ret = new Code(size(),this);
-		final int size = size();
-		int num = c;
-		int rest;
+		final Code ret  = new Code(size(), this);
+		final int  size = size();
+		int        num  = c;
+		int        rest;
 
 		for (int i = size - 1; i >= 0; i--)
 		{
-			NodeContext nc = this.get(i);
-			CodeContext ctx = nc.getContext();
-			final int base = ctx.size();
-			rest = num % base;
-			num /= base;
+			NodeContext nc   = this.get(i);
+			CodeContext ctx  = nc.getContext();
+			final int   base = ctx.size();
+			rest  = num % base;
+			num  /= base;
 			ret.setCode(i, rest);
+
+			if (num == 0)
+				break;
 		}
 		return ret;
 	}
 
 	/**
-	 * 
 	 * @param codes
-	 *            Intervalle des codes à générer
+	 *              Intervalle des codes à générer
 	 * @return L'ensemble des codes de l'intervalle
+	 * @deprecated
 	 */
 	public Code[] generateAllCodes(Interval codes)
 	{
 		final Code[] ret = new Code[(int) codes.size()];
-		final int c = (int) codes.getb();
-		int i = (int) codes.geta();
+		final int    c   = (int) codes.getb();
+		int          i   = (int) codes.geta();
 
 		for (int o = 0; i <= c; i++, o++)
 		{
@@ -123,11 +124,12 @@ public class Encoding extends ArrayList<NodeContext>
 
 	/**
 	 * @return tous les codes possibles
+	 * @deprecated
 	 */
 	public Code[] generateAllCodes()
 	{
-		final int totalNbState = getTotalNbStates();
-		final Code codes[] = new Code[totalNbState];
+		final int  totalNbState = getTotalNbStates();
+		final Code codes[]      = new Code[totalNbState];
 
 		for (int i = 0; i < totalNbState; i++)
 		{
